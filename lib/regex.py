291c533cd9781ac048d3e1a8cc53ef5a7eb9aaa1
danwycliff <danwycliff6769@gmail.com>
2023-09-22 11:32:48 +0300

    codes

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env pipenv
 import re
 
-my_pattern = r""
+my_pattern = r"It's such a lovely day today."
 my_regex = re.compile(my_pattern)
-
+match = re.search("Some weather we're having today, huh?",my_pattern)
+print(match)
